3015512e8d1e8d317e6c490f6127a18d963d90ed
lia <martynovaliya@gmail.com>
2024-11-25 10:35:07 +0300

    async test login

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,15 +2,19 @@ from unittest.mock import AsyncMock, patch
 
 import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
 from src.main import app
 from database.actions.with_token import add_token
 from database.actions.with_user import select_user, login, get_role_by_login
 client = TestClient(app)
 
+@pytest.mark.asyncio
 def test_login_get():
-    response = client.get('/login')
-    assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to login page!"}
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get('/login')
+        assert response.status_code == 200
+        assert response.json() == {"message": "Welcome to login page!"}
 
 
 
@@ -30,40 +34,44 @@ def mock_dependencies(mocker):
         "add_token": mock_add_token,
     }
 
-
+@pytest.mark.asyncio
 def test_login_success(mock_dependencies):
-    mock_dependencies["select_user"].return_value = True
-    mock_dependencies["login"].return_value = True
-    mock_dependencies["get_role_by_login"].return_value = "admin"
-    mock_dependencies["add_token"].return_value = None
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        mock_dependencies["select_user"].return_value = True
+        mock_dependencies["login"].return_value = True
+        mock_dependencies["get_role_by_login"].return_value = "admin"
+        mock_dependencies["add_token"].return_value = None
 
-    response = client.post("/login", json={"login": "lia", "password": "lia"})
-    assert response.status_code == 200
-    assert response.json()["message"] == "success"
+        response = await client.post("/login", json={"login": "lia", "password": "lia"})
+        assert response.status_code == 200
+        assert response.json()["message"] == "success"
 
-
+@pytest.mark.asyncio
 def test_login_incorrect_password(mock_dependencies):
-    mock_dependencies["select_user"].return_value = True
-    mock_dependencies["login"].return_value = False
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        mock_dependencies["select_user"].return_value = True
+        mock_dependencies["login"].return_value = False
 
-    response = client.post("/login", json={"login": "lia", "password": "wrong_password"})
-    assert response.status_code == 200
-    assert response.json()["message"] == "Password is incorrect"
+        response = await client.post("/login", json={"login": "lia", "password": "wrong_password"})
+        assert response.status_code == 200
+        assert response.json()["message"] == "Password is incorrect"
 
-
+@pytest.mark.asyncio
 def test_login_user_not_found(mock_dependencies):
-    mock_dependencies["select_user"].return_value = False
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        mock_dependencies["select_user"].return_value = False
 
-    response = client.post("/login", json={"login": "unknown_user", "password": "any_password"})
-    assert response.status_code == 200
-    assert response.json()["message"] == "There's no user with login 'unknown_user'. Try a different one"
+        response = await client.post("/login", json={"login": "unknown_user", "password": "any_password"})
+        assert response.status_code == 200
+        assert response.json()["message"] == "There's no user with login 'unknown_user'. Try a different one"
 
-
+@pytest.mark.asyncio
 def test_login_internal_error(mock_dependencies, mocker):
-    mocker.patch("src.routes.login.select_user", side_effect=Exception("Database connection error"))
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        mocker.patch("src.routes.login.select_user", side_effect=Exception("Database connection error"))
 
-    mock_dependencies["select_user"].side_effect = Exception("Database connection error")
+        mock_dependencies["select_user"].side_effect = Exception("Database connection error")
 
-    response = client.post("/login", json={"login": "lia", "password": "lia"})
-    assert response.status_code == 200
-    assert response.json()["message"].startswith("An error ")
+        response = await client.post("/login", json={"login": "lia", "password": "lia"})
+        assert response.status_code == 200
+        assert response.json()["message"].startswith("An error ")
