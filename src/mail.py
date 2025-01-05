9f52572a7e09c0dd5f377337bd4861abc97850ee
lia <martynovaliya@gmail.com>
2025-01-05 21:48:47 +0300

    tochno

--- src/mail.py
+++ src/mail.py
@@ -4,36 +4,35 @@ from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 import os
 
-# Конфигурация
+
 
 load_dotenv()
-SMTP_SERVER = "smtp.gmail.com"  # Адрес SMTP сервера (например, для Gmail)
-SMTP_PORT = 587  # Порт для SMTP сервера
-EMAIL = os.getenv("EMAIL")  # Ваш адрес электронной почты
-PASSWORD = os.getenv("PASSWORD_EMAIL")  # Пароль или App Password для почты
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD_EMAIL")
 
-# Функция отправки письма
+
 def send_email(to_email, subject, body):
     try:
-        # Создание сообщения
+
         msg = MIMEMultipart()
         msg["From"] = EMAIL
         msg["To"] = to_email
         msg["Subject"] = subject
 
-        # Добавление текста письма
+
         msg.attach(MIMEText(body, "plain"))
 
-        # Установка соединения с SMTP-сервером
+
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()  # Шифрование соединения
-            server.login(EMAIL, PASSWORD)  # Аутентификация
-            server.sendmail(EMAIL, to_email, msg.as_string())  # Отправка письма
+            server.starttls()
+            server.login(EMAIL, PASSWORD)
+            server.sendmail(EMAIL, to_email, msg.as_string())
 
         print("Письмо успешно отправлено!")
     except Exception as e:
         print(f"Ошибка при отправке письма: {e}")
 
 
-# Использование функции
 send_email("pup0000000chek@gmail.com", "Лия", "Красотка")
